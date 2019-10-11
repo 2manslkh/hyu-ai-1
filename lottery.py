1da2e23d93d24d92c271b6a424a09137948c2ddd
Keng Hin <kenghin_lim@hotmail.com>
2019-10-11 14:47:31 +0900

    'v1.0'

--- lottery.py
+++ lottery.py
@@ -8,7 +8,9 @@ data = pd.read_csv('lottery.csv')
 lottery_slots = ['first','second','third','fourth','fifth','sixth','bonus']
 
 # Initialize frequency Dataframe
-frequency_df =  pd.DataFrame(columns=['count'],index=range(1,46))
+frequency_df = pd.DataFrame(columns=['count'],index=range(1,46))
+
+# Replace NaN Values with 0
 frequency_df = frequency_df.fillna(0)
 
 # Tabluate count
@@ -17,29 +19,79 @@ for slot in lottery_slots:
     for key in slot_data.keys():
         frequency_df['count'][key] += slot_data[key]
 
-print(frequency_df)
-
+# Sort numbers in descending count order
 frequency_df = frequency_df.sort_values('count',ascending=False)
-print(frequency_df)
 
+# TASK-2: Display the most frequently appeared number to the least
 for k,v in frequency_df.iterrows():
     n = v['count']
     print(f"{k} -> {n}")
 
-# print(frequency_d)
-# n = sum(frequency_d.values())
-# print(n==7*716)
-# frequency_d[1] = data['first'].value_counts()
-# frequency_d[2] = data['second'].value_counts()
-# frequency_d[3] = data['third'].value_counts()
-# frequency_d[4] = data['fourth'].value_counts()
-# frequency_d[5] = data['fifth'].value_counts()
-# frequency_d[6] = data['sixth'].value_counts()
-# frequency_d[7] = data['bonus'].value_counts()
+# TASK-3: Create modified lottery data format by adding at new columns.
+# Store slot names
+lottery_slots = ['first','second','third','fourth','fifth','sixth']
+# 1. FEATURE-1 SUM OF DIFFERENCES
+# Sum of difference between adjacent numbers
+# (e.g [1 6 11 16 21 26] = (6 - 1) + (11 - 6) + (16 - 11) + (21 - 16) + (26 - 21) = 25)
+import numpy as np
+def sum_of_differences(df):
+    x = df[lottery_slots[5]] - df[lottery_slots[4]]
+    for i in range(4,0,-1):
+        x += df[lottery_slots[i]] - df[lottery_slots[i-1]]
+    return x
 
-# print(data[['first','second']].value_counts())
-# print(data[['first','second']].keys())
-# print(data['first'].value_counts().keys())
-# print(frequency_d)
-# print(type(frequency_d))
+data['sum_of_differences'] = sum_of_differences(data)
 
+# 2. FEATURE-2 MEAN OF NUMBERS
+data['mean'] = data[lottery_slots].apply(np.mean,axis=1)
+
+# 3. FEATURE-3 QUARTER OF YEAR
+import datetime
+def get_quarter(x):
+    ''' Return which quarter of the year the draw was made'''
+    x = x.split('.')
+    x = list(map(int,x))
+    m = x[1]
+    if 1 <= m <= 3:
+        return 1
+    elif 4 <= m <= 6:
+        return 2
+    elif 7 <= m <= 9:
+        return 3
+    elif 10 <= m <= 12:
+        return 4
+
+data['quarter'] = data['date'].apply(get_quarter)
+
+# Print First 20 Lines
+print(data.head(20))
+
+# TASK-4 K-mean clustering – Use any combination of features in your lottery.csv to group all weekly rounds
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# FEAUTURES USED
+feature_names = ['sum_of_differences','mean','quarter']
+X = data[feature_names]
+
+title = 'lottery_clusters_4'
+estimator = KMeans(n_clusters=4)
+
+fig = plt.figure(1, figsize=(4, 3))
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+estimator.fit(X)
+labels = estimator.labels_
+
+ax.scatter(X[feature_names[0]], X[feature_names[1]], X[feature_names[2]],
+            c=labels.astype(np.float), edgecolor='k')
+
+ax.w_xaxis.set_ticklabels([])
+ax.w_yaxis.set_ticklabels([])
+ax.w_zaxis.set_ticklabels([])
+ax.set_xlabel(feature_names[0])
+ax.set_ylabel(feature_names[1])
+ax.set_zlabel(feature_names[2])
+ax.set_title(title)
+ax.dist = 12
+plt.show()
